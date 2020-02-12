c1d2d59107432dff2f7c78f9721bf5ada2c781e4
David Sato <david.sato64@gmail.com>
2020-02-12 19:00:00 +0530

    useWraptor useEffect API check

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { WraptorComponent } from '@w3stside/wraptor'
+
 function App() {
+  console.debug('window.web3', window.web3)
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +13,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <WraptorComponent 
+          type="ETH"
+          provider={window.web3}
+          contractAddress="0xc778417E063141139Fce010982780140Aa0cD5Ab"
+          userAddress="0xfa3a5ba1864C4567aC77D50EcD91a2AaE92B650D"
+        />
       </header>
     </div>
   );
