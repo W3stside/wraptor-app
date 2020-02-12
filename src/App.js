67c9205fda0051c7563ca9082c8b664d0b8c7bcf
David Sato <david.sato64@gmail.com>
2020-02-12 20:14:24 +0530

    updates ad logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
-import { WraptorComponent } from '@w3stside/wraptor'
+import { WraptorComponent, useWeb3Setup } from '@w3stside/wraptor'
 
 function App() {
-  console.debug('window.web3', window.web3)
+  const { web3, error } = useWeb3Setup()
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <WraptorComponent 
+        {error && <div style={{ background: '#c15050', color: 'black', padding: '0 30px' }}><p>An error has occurred :(</p><p>Error message: {error.message}</p></div>}
+        {web3 ? <WraptorComponent 
           type="ETH"
-          provider={window.web3}
+          provider={web3}
           contractAddress="0xc778417E063141139Fce010982780140Aa0cD5Ab"
           userAddress="0xfa3a5ba1864C4567aC77D50EcD91a2AaE92B650D"
-        />
+          customStyle={`
+            color: black;
+            font-weight: bolder;
+            width: 60%;
+          `}
+        /> : <div>Waiting for Web3 to initiate...</div>}
       </header>
     </div>
   );
